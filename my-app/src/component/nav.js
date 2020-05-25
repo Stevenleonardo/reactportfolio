d5a7f3d783cd7618249f95f90bf13b9d8f30e70d
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-25 11:25:57 -0400

    nav effect function when scrolling

--- my-app/src/component/nav.js
+++ my-app/src/component/nav.js
@@ -6,7 +6,21 @@ class Navbar extends React.Component{
     }
 }
 
-componentDidMount() {
+function componentDidMount() {
 }
 
-navEffect() 
+function navEffect() {
+window.addEventListener("scroll", ()=>{
+    var navBar = document.getElementById("navbar");
+     var domRect = navBar.getBoundingClientRect();
+     var background = document.getElementById("background");
+     var domBackground = background.getBoundingClientRect();
+
+     if (domRect.y <= -domRect.height){
+         navBar.classList.add("fade-in-nav");
+     }
+     if (-domRect.height < domBackground.top){
+         navBar.classList.remove("fade-in-nav");
+     }
+});
+}
