5f046e46ae7aebc0a1012b88455b583164ec04f2
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-26 10:44:16 -0400

    css added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Navbar from "./component/navbar.jsx";
-import Footer from "./component/footer.jsx";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./styles/index.scss";
 
