d711e92ff0bde3016446f1120d648af1961a97e8
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-26 10:28:59 -0400

    info added

--- my-app/src/component/background.js
+++ my-app/src/component/background.js
@@ -4,5 +4,34 @@ import PropTypes from "prop-types"
 
 
 export default class background extends React.Component {
-
+    render(){
+    return (
+        <div id="my-background" className="background">
+            <div id="stars" />
+            <div id="stars2" />
+            <div id="stars3" />
+            <div className="top-container flex">
+                <h1>
+                    Hello, I&apos;m Frank Izquierdo.
+                    <br />
+                    I&apos;m a Full Stack Web Developer.
+                </h1>
+                <button
+                    className={"work-button " + this.props.bounceIn}
+                    data-wow-offset="0"
+                    onClick={() => {
+                        let node = document.getElementById(
+                            this.props.aboutRef.current.props.id
+                        );
+                        window.scrollTo({
+                            top: node.offsetTop,
+                            behavior: "smooth"
+                        });
+                    }}>
+                    View my work
+                </button>
+            </div>
+        </div>
+    );
+}
 }
