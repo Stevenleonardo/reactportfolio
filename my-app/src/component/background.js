88d2f26f92a9284fa80c7a3e0c388a06b7c1092c
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-26 10:30:07 -0400

    props ran threw

--- my-app/src/component/background.js
+++ my-app/src/component/background.js
@@ -4,34 +4,38 @@ import PropTypes from "prop-types"
 
 
 export default class background extends React.Component {
-    render(){
-    return (
-        <div id="my-background" className="background">
-            <div id="stars" />
-            <div id="stars2" />
-            <div id="stars3" />
-            <div className="top-container flex">
-                <h1>
-                    Hello, I&apos;m Frank Izquierdo.
+    render() {
+        return (
+            <div id="my-background" className="background">
+                <div id="stars" />
+                <div id="stars2" />
+                <div id="stars3" />
+                <div className="top-container flex">
+                    <h1>
+                        Hello, I&apos;m Steven Leonardo.
                     <br />
                     I&apos;m a Full Stack Web Developer.
                 </h1>
-                <button
-                    className={"work-button " + this.props.bounceIn}
-                    data-wow-offset="0"
-                    onClick={() => {
-                        let node = document.getElementById(
-                            this.props.aboutRef.current.props.id
-                        );
-                        window.scrollTo({
-                            top: node.offsetTop,
-                            behavior: "smooth"
-                        });
-                    }}>
-                    View my work
+                    <button
+                        className={"work-button " + this.props.bounceIn}
+                        data-wow-offset="0"
+                        onClick={() => {
+                            let node = document.getElementById(
+                                this.props.aboutRef.current.props.id
+                            );
+                            window.scrollTo({
+                                top: node.offsetTop,
+                                behavior: "smooth"
+                            });
+                        }}>
+                        View my work
                 </button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-}
+Background.propTypes = {
+	aboutRef: PropTypes.object,
+	bounceIn: PropTypes.string
+};
