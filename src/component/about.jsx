1939f6970d0c7b4fb0bbecacaec19a340fa4a474
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-26 16:57:47 -0400

    updating portfolio information

---
+++ src/component/about.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import layers from "../img/layers.png";
+import cogwheel from "../img/cogwheel.png";
+import pen from "../img/pen.png";
+import browser from "../img/browser.png";
+//import myImage from "../img/myImage.png";
+import PropTypes from "prop-types";
+
+export default class About extends React.Component {
+	render() {
+		return (
+			<div
+				id="about-container"
+				className="content-containers container text-center mt-5">
+				<h1 id="about" className={this.props.bounceLeft}>
+					About
+				</h1>
+				<div className="row mt-5">
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInLeft
+						}>
+						<img src={layers} alt=""/>
+						<h4>Responsive</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInLeft
+						}>
+						<img src={pen} alt=""/>
+						<h4>Design</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInRight
+						}>
+						<img src={cogwheel} alt=""/>
+						<h4>Performance</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInRight
+						}>
+						<img src={browser} alt=""/>
+						<h4>Agile</h4>
+					</div>
+				</div>
+
+				<div className="row" style={{ marginTop: 7 + "rem" }}>
+					<div className="col-12 col-lg-6 hidden">
+						<img
+							className={"img-fluid " + this.props.fadeIn}
+							//src={myImage}
+							alt=""
+							style={{
+								borderRadius: 50 + "%",
+								height: 250 + "px",
+								width: 250 + "px"
+							}}
+						/>
+						<p className={"mytext pt-3 " + this.props.fadeIn}>
+						 I am a full stack developer that trained
+                    and learned at UCF. I have created projects that derail from the front end development 
+                    such as React and CSS3 to the backend with MongoDB
+                    
+						</p>
+					</div>
+						</div>
+					</div>
+		);
+	}
+}
+
+About.propTypes = {
+	bounceLeft: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
+};
